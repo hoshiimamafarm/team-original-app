d829c0f2197d8f4d55868149f23b5bb9d0f346d4
msk821 <misaki.nakamura1@gmail.com>
2023-06-08 11:53:09 +0900

    直しました

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>|
-    <router-link to="/Push">PushPr</router-link>
+    <router-link to="/Push">postselfpr</router-link>
   </nav>
   <router-view />
 </template>
--- src/views/PushPrView.vue
+++ src/views/PushPrView.vue
@@ -1,33 +1,29 @@
 <template>
   <h1>自己PR投稿</h1>
-  <div class="home__wrapper">
-    <div class="title_container">
+  <div>
+    <div>
       <textarea v-model="title_text" placeholder="タイトル"></textarea>
     </div>
-    <div class="pr_container">
+    <div>
       <textarea v-model="pr_text" placeholder="自己PRの内容"></textarea>
     </div>
     <div v-if="errors">
       <div v-for="error in errors" v-bind:key="error">{{ error }}</div>
     </div>
-    <div class="push-button">
-      <button v-on:click="pushButton">投稿</button>
+    <div>
+      <button v-on:click="postSelfPrButton">投稿</button>
     </div>
   </div>
-  <ul
-    class="pr-list_container"
-    v-for="(text, index) in texts"
-    v-bind:key="index"
-  >
-    <li class="pr">
+  <ul v-for="(selfPr, index) in selfPrs" v-bind:key="index">
+    <li>
       <div>
-        {{ text.title }}
+        {{ selfPr.title }}
       </div>
       <div>
-        {{ text.pr }}
+        {{ selfPr.selfPr }}
       </div>
       <div class="deleteButton">
-        <button v-on:click="deleteButton(index)">削除</button>
+        <button v-on:click="deleteSelfPrButton(index)">削除</button>
       </div>
     </li>
   </ul>
@@ -35,7 +31,13 @@
 </template>
 
 <script>
-import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "../firebase.js";
 
 export default {
@@ -43,32 +45,52 @@ export default {
     return {
       title_text: "",
       pr_text: "",
-      texts: [],
+      selfPrs: [],
       errors: [],
     };
   },
   methods: {
-    async pushButton() {
-      await addDoc(collection(db, "selfPrs"), {
-        title: this.title_text,
-        selfPr: this.pr_text,
+    async fetchdata() {
+      const querySnapshot = await getDocs(collection(db, "selfPrs"));
+      this.selfPrs = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        console.log(doc.data().title);
+        console.log(doc.data().selfPr);
+        this.selfPrs.push({
+          title: doc.data().title,
+          selfPr: doc.data().selfPr,
+          id: doc.id,
+        });
       });
-      if (!this.pr_text) {
+    },
+
+    async postSelfPrButton() {
+      if (!this.pr_text && !this.title_text) {
+        this.errors.push("タイトルと内容を入れてください");
+      } else if (!this.pr_text) {
         this.errors.push("内容を入れてください");
       } else if (!this.title_text) {
         this.errors.push("タイトルを入れてください");
       } else {
-        this.texts.push({
+        await addDoc(collection(db, "selfPrs"), {
           title: this.title_text,
-          pr: this.pr_text,
+          selfPr: this.pr_text,
         });
+
+        this.fetchdata();
         (this.title_text = ""), (this.pr_text = "");
         this.errors.splice(0);
       }
     },
+    async deletedata(index) {
+      await deleteDoc(doc(db, "selfPrs", this.selfPrs[index].id));
+      this.selfPrs.splice(index, 1);
+    },
 
-    deleteButton(index) {
-      this.texts.splice(index, 1);
+    deleteSelfPrButton(index) {
+      this.deletedata(index);
     },
   },
 };
